85f66eedf38d85ff3c38adb550cbfe84948328c9
romelaazizyan <49792180+romelaazizyan@users.noreply.github.com>
2019-04-19 11:13:28 -0600

    Create newJbuttonColor.java

---
+++ newJbuttonColor.java
@@ -0,0 +1,16 @@
+package masterMindGame;
+
+import javax.swing.JButton;
+
+public class newJbuttonColor extends JButton {
+	private int colorIndex;
+
+	public int getColorIndex() {
+		return colorIndex;
+	}
+
+	public void setColorIndex(int colorIndex) {
+		this.colorIndex = colorIndex;
+	}
+
+}
